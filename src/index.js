9f9648c09085e6c95d579fd731a5235da9c9535c
yangtao <istaotao@aliyun.com>
2019-07-31 11:53:42 +0800

    Add css-loader and style-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'loadsh';
 
+import './index.css';
+
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
   return element;
 }
 
