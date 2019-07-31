487b4eb4e70e30b03be27b5d473f650785e91646
yangtao <istaotao@aliyun.com>
2019-07-31 15:04:23 +0800

    Add webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
