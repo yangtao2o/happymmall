6aae5839654a940f991b8619e80f0182006808c7
yangtao <istaotao@aliyun.com>
2019-11-01 16:44:50 +0800

    add: error page

--- src/app.jsx
+++ src/app.jsx
@@ -14,6 +14,7 @@ import Home from "page/home/index.jsx";
 import About from "page/about/index.jsx";
 import User from "page/user/index.jsx";
 import Login from "page/login/index.jsx";
+import Error from "page/error/index.jsx";
 
 class App extends React.Component {
   render() {
@@ -31,7 +32,7 @@ class App extends React.Component {
                   <Route path="/order" component={Home} />
                   <Route path="/about/" component={About} />
                   <Route path="/user/" component={User} />
-                  <Redirect from="*" to="/" />
+                  <Route component={Error} />
                 </Switch>
               </Layout>
             )} />
